680ac7fedf3e22eb0b27f72febba2af5620939d4
Gedian_G_Funka <gediangabrielf@gmail.com>
2026-05-16 07:02:54 -0300

    GG

---
+++ src/main/java/com/example/Exercicio_Thymeleaf/ExercicioThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.example.Exercicio_Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioThymeleafApplication.class, args);
+	}
+
+	//Gedian gabriel f
+}
---
+++ src/test/java/com/example/Exercicio_Thymeleaf/ExercicioThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exercicio_Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
